857ed498b3b5aff7f71ea6cab88a08839bb84f04
Andy Liu <andyliu4002@gmail.com>
2023-11-07 20:12:50 -0800

    Finished A4

--- A4/CA4.m
+++ A4/CA4.m
@@ -6,7 +6,6 @@ load('CA4matrix2.mat');
 
 expA50 = exp_approx(A, 50);
 expA150 = exp_approx(A, 150);
-
 imagesc(real(expA50));
 colormap gray;
 imagesc(real(expA150));
@@ -21,9 +20,13 @@ for i = 1:length(k_values)
 end 
 
 plot(k_values, cpu_time);
-xlabel('k terms');
-ylabel('CPU Time (seconds)');
+xlabel('k terms', 'FontSize', 14);
+ylabel('CPU Time (seconds)', 'FontSize', 14);
 title('CPU Time vs. Number of Terms in Series');
+
+tic;
+expm(A);
+expm_time = toc; 
 
 err = zeros(length(k_values), 1);
 expAexact = expm(A);
@@ -32,11 +35,6 @@ for i = 1:length(k_values)
     err(i) = norm(expAexact-expAk, 2); 
 end
 semilogy(k_values, err);
-xlabel('k terms');
-ylabel('Error (2-norm)');
+xlabel('k terms', 'FontSize', 14);
+ylabel('Error (2-norm)', 'FontSize', 14);
 title('Error vs. Number of Terms in Series');
-
-tic;
-expm(A);
-expm_time = toc; 
-
