fdde7151fd4814bbe8f12075fc5959a7808e58ff
AndyLiu314 <57054829+AndyLiu314@users.noreply.github.com>
2023-11-24 18:46:34 -0800

    Update CA4.m

--- A4/CA4.m
+++ A4/CA4.m
@@ -11,30 +11,30 @@ colormap gray;
 imagesc(real(expA150));
 colormap gray;
 
-k_values = 5:5:150;
-cpu_time = zeros(length(k_values), 1);
-for i = 1:length(k_values)
-    tic;
-    exp_approx(A, k_values(i));
-    cpu_time(i) = toc;
-end 
-
-plot(k_values, cpu_time);
-xlabel('k terms', 'FontSize', 14);
-ylabel('CPU Time (seconds)', 'FontSize', 14);
-title('CPU Time vs. Number of Terms in Series');
-
-tic;
-expm(A);
-expm_time = toc; 
-
-err = zeros(length(k_values), 1);
-expAexact = expm(A);
-for i = 1:length(k_values)
-    expAk = exp_approx(A, k_values(i));
-    err(i) = norm(expAexact-expAk, 2); 
-end
-semilogy(k_values, err);
-xlabel('k terms', 'FontSize', 14);
-ylabel('Error (2-norm)', 'FontSize', 14);
-title('Error vs. Number of Terms in Series');
+% k_values = 5:5:150;
+% cpu_time = zeros(length(k_values), 1);
+% for i = 1:length(k_values)
+%     tic;
+%     exp_approx(A, k_values(i));
+%     cpu_time(i) = toc;
+% end 
+% 
+% plot(k_values, cpu_time);
+% xlabel('k terms', 'FontSize', 14);
+% ylabel('CPU Time (seconds)', 'FontSize', 14);
+% title('CPU Time vs. Number of Terms in Series');
+% 
+% tic;
+% expm(A);
+% expm_time = toc; 
+% 
+% err = zeros(length(k_values), 1);
+% expAexact = expm(A);
+% for i = 1:length(k_values)
+%     expAk = exp_approx(A, k_values(i));
+%     err(i) = norm(expAexact-expAk, 2); 
+% end
+% semilogy(k_values, err);
+% xlabel('k terms', 'FontSize', 14);
+% ylabel('Error (2-norm)', 'FontSize', 14);
+% title('Error vs. Number of Terms in Series');
